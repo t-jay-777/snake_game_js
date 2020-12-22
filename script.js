58a6c2926948f8934899f3ff4ebf747ff25b514b
Yevhen Tkachenko <joy_123@ukr.net>
2020-12-22 23:30:16 +0100

    script.js add variables

---
+++ script.js
@@ -0,0 +1,26 @@
+//document.querySelector(".className")
+//select first element by className 
+let grid = document.querySelector(".grid");
+let popup = document.querySelector(".popup");
+let playAgain = document.querySelector(".playAgain");
+let scoreDisplay = document.querySelector(".scoreDisplay") 
+let left = document.querySelector(".left") 
+let bottom = document.querySelector(".bottom") 
+let right = document.querySelector(".right") 
+let up = document.querySelector(".top") 
+let widt = 10; 
+let currentIndex = 0 
+let appleIndex = 0 
+let currentSnake = [2,1,0] 
+let direction = 1 
+let score = 0 
+let speed = 0.8 
+let intervalTime = 0 
+let interval = 0
+//
+document.addEventListener("DOMContentLoaded",function(){ 
+    document.addEventListener("keyup",control) 
+    createBoard() 
+    startGame() 
+    playAgain.addEventListener("click", replay); 
+    })
