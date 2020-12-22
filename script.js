8bf4c3fc1f418d884c0e1c099d9fd69cea301c51
Yevhen Tkachenko <joy_123@ukr.net>
2020-12-22 23:35:01 +0100

    script.js a game board builder function

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 //document.querySelector(".className")
-//select first element by className 
+//select first element by className
 let grid = document.querySelector(".grid");
 let popup = document.querySelector(".popup");
 let playAgain = document.querySelector(".playAgain");
-let scoreDisplay = document.querySelector(".scoreDisplay") 
-let left = document.querySelector(".left") 
-let bottom = document.querySelector(".bottom") 
-let right = document.querySelector(".right") 
-let up = document.querySelector(".top") 
-let widt = 10; 
-let currentIndex = 0 
-let appleIndex = 0 
-let currentSnake = [2,1,0] 
-let direction = 1 
-let score = 0 
-let speed = 0.8 
-let intervalTime = 0 
-let interval = 0
-//
-document.addEventListener("DOMContentLoaded",function(){ 
-    document.addEventListener("keyup",control) 
-    createBoard() 
-    startGame() 
-    playAgain.addEventListener("click", replay); 
-    })
+let scoreDisplay = document.querySelector(".scoreDisplay");
+let left = document.querySelector(".left");
+let bottom = document.querySelector(".bottom");
+let right = document.querySelector(".right");
+let up = document.querySelector(".top");
+let widt = 10;
+let currentIndex = 0;
+let appleIndex = 0;
+let currentSnake = [2, 1, 0];
+let direction = 1;
+let score = 0;
+let speed = 0.8;
+let intervalTime = 0;
+let interval = 0;
+
+//this event will start immediately once the HTML content is loaded on our screen
+document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("keyup", control);
+  createBoard();
+  startGame();
+  playAgain.addEventListener("click", replay);
+});
+
+//create a game board
+function createBoard() {
+  popup.style.display = "none";
+  for (let i = 0; i < 100; i++) {
+    let div = document.createElement("div");
+    grid.appendChild(div);
+  }
+}
+
+
