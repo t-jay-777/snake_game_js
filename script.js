8b8d20199d3ac96ae24167a9d15a3149399e8064
Yevhen Tkachenko <joy_123@ukr.net>
2020-12-27 17:47:56 +0100

    script.js add eatApplefunction

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function moveSnake(squares) {
     // movement ends here  
     eatApple(squares, tail)
     squares[currentSnake[0]].classList.add("snake")
-} 
+}
 /*Let's assume that our snake just started moving and is facing to 
 the right (that is, direction = 1). That direction will be added to 
 the currentSnake's head and the sum will be pushed as the new snakeHead.
@@ -97,6 +97,9 @@ which moves our snake a step forward to the right after one second.
 //true (meaning we hit something) or false
 function checkForHits(squares) {
     if (
+        // currentSnake [0] (the head of the snake) + width (10) is 
+        // equal to the total area of the width (that is, width*width = 100) and 
+        // the direction is equal to the width.
         //currentSnake [0] the head of the snake
         (currentSnake[0] + width >= (width * width) && direction === width) ||
         (currentSnake[0] % width === width - 1 && direction === 1) ||
@@ -104,9 +107,28 @@ function checkForHits(squares) {
         (currentSnake[0] - width <= 0 && direction === -width) ||
         squares[currentSnake[0] + direction].classList.contains("snake")
     ) {
-        return true
+        return true;
     } else {
-        return false
+        return false;
+    }
+}
+
+// The eatApple function is called from 
+// the moveSnake function every time the snake moves a step.
+// It receives two argument squares, .grid div and tail (basically 
+//     the value that was popped up from the snake in moveOutcome). 
+//     It then checks if the next position our snake moves to contains an apple.
+function eatApple(squares, tail) {
+    if (squares[currentSnake[0]].classList.contains("apple")) {
+        squares[currentSnake[0]].classList.remove("apple");
+        squares[tail].classList.add("snake");
+        currentSnake.push(tail);
+        randomApple(squares);
+        score++;
+        scoreDisplay.textContent = score;
+        clearInterval(interval);
+        intervalTime = intervalTime * speed;
+        interval = setInterval(moveOutcome, intervalTime);
     }
 }
 
